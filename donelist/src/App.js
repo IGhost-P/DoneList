ca813565f7bd9e539146765ba5370e0a00998d43
IGhos-P <dndb3599@gmail.com>
2021-07-19 23:33:40 +0900

    시작이야

---
+++ donelist/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Loding from './component/Loding';
+
+function App() {
+  return (
+    <Loding />
+  );
+}
+
+export default App;
---
+++ donelist/src/component/Loding.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function Loding() {
+    const [input, inputState] = useState
+
+    return (
+        <div>
+            입력칸입니다<input />
+        </div>
+    )
+}
+
+export default Loding
