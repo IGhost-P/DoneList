981dab801ed68cfa195b49c611d8863c47b05dac
IGhos-P <dndb3599@gmail.com>
2021-07-20 21:06:09 +0900

    몰것다 ㅠㅠ 일단 버튼 추가는 하는데 텍스트가 안됨

--- donelist/src/App.js
+++ donelist/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Loding from './component/Loding';
 
--- donelist/src/component/Loding.js
+++ donelist/src/component/Loding.js
@@ -1,11 +1,34 @@
 import React, { useState } from 'react'
 
 function Loding() {
-    const [input, inputState] = useState
+    const [list, listState] = useState(
+        [
+            { id: 1, text: '시발' }
+        ]);
+
+    const [inputText, setinputText] = useState('');
+    const [nextId, setNextId] = useState(2);
+    const onChange = e => setinputText(e.target.value);
+    const onClick = () => {
+        listState(
+            [
+                ...list,
+                inputText
+            ])
+    }
+    const doneList = list.map(done => <li key={done.id}>{done.text}</li>)
+
 
     return (
         <div>
-            입력칸입니다<input />
+
+            입력칸입니다<input
+                value={inputText}
+                placeholder="일정을 입력하세요"
+                onChange={onChange}
+            />
+            <button onClick={onClick}>추가</button>
+            <ul>{doneList}</ul>
         </div>
     )
 }
