aa9beb2374fc5d0a30f54ced19f806c97ad9168b
IGhos-P <dndb3599@gmail.com>
2021-07-27 20:51:05 +0900

    몰것다 시벌

--- donelist/src/component/Loding.js
+++ donelist/src/component/Loding.js
@@ -1,43 +1,52 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
+
+
+const reducuer = (state, action) => {
+    switch (action.type) {
+        case "ONCHANGE":
+            return {
+                ...state,
+                [action.name]: action.value
+            }
+        case "ONClICK":
+            return {
+
+            }
+    }
+}
 
 function Loding() {
-    const [list, listState] = useState([
 
-        { id: 1, text: '안녕' }
-    ]);
 
-    const [inputText, setinputText] = useState('');
-    const [nextId, setNextId] = useState(2);
-    const onChange = e => setinputText(e.target.value);
-    console.log(inputText);
-    console.log(nextId);
-    const { id, text } = list;
-    const onClick = () => {
-        const nextList = {
-            ...list,
-            [id]: nextId,
-            [text]: inputText
-        }
-        listState(nextList)
-        setinputText('')
-        console.log(list);
-        console.log(nextList);
-
+    const [state, dispatch] = useReducer(reducuer, {
+        name: '',
+        nickName: ''
+    });
+    const { name, nickName } = state
+    const onClick = e => {
+        dispatch(e.target);
     }
-
-    const doneList = list.map(done => <li key={done.id}>{done.text}</li>)
-
 
     return (
         <div>
-
-            입력칸입니다<input
-                value={inputText}
-                placeholder="일정을 입력하세요"
-                onChange={onChange}
-            />
-            <button onClick={onClick}>추가</button>
-            <ul>{doneList}</ul>
+            <div>
+                <input
+                    name="name"
+                    value={name}
+                    onChange={onClick}
+                />
+                <input
+                    name="nickName"
+                    value={nickName}
+                    onChange={onClick}
+                />
+            </div>
+            <div>
+                <b>이름 :</b> {name}
+            </div>
+            <div>
+                <b>닉네임 : </b> {nickName}
+            </div>
         </div>
     )
 }
