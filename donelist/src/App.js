b5be17635440c75e70045fcf9e41c4546b03f881
IGhos-P <dndb3599@gmail.com>
2021-08-17 16:52:57 +0900

    추가 기능 구현 완료

--- donelist/src/App.js
+++ donelist/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import Loding from './component/Loding';
 import TodoHead from './component/TodoHead';
@@ -25,10 +25,24 @@ function App() {
       checked: false,
     },
   ])
+
+  const nextId = useRef(4);
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  )
   return (
     <>
       <TodoTemplate>
-        <TodoInsert />
+        <TodoInsert onInsert={onInsert} />
         <TodoList todos={todos} /> {/* todos에 위에 만든 리스트를 넣어주자 */}
       </TodoTemplate>
     </>
