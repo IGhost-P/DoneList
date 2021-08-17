0b2a0e344041241d96e7a58dd1f60b60cdbe69d4
IGhos-P <dndb3599@gmail.com>
2021-08-17 16:18:35 +0900

    10-14 데이터 전달 완료

--- donelist/src/App.js
+++ donelist/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import Loding from './component/Loding';
 import TodoHead from './component/TodoHead';
@@ -8,11 +8,28 @@ import TodoList from './component/TodoList';
 
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리엑트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링 해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ])
   return (
     <>
       <TodoTemplate>
         <TodoInsert />
-        <TodoList />
+        <TodoList todos={todos} /> {/* todos에 위에 만든 리스트를 넣어주자 */}
       </TodoTemplate>
     </>
   );
